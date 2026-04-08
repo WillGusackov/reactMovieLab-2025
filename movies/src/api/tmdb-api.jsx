6dde521bc6dd14d8c96099ae5878a59c11f9031a
Will <20107603@mail.wit.ie>
2026-04-08 15:41:04 +0100

    Refactor: Favourite movie list now stored in a context.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -53,6 +53,23 @@ export const getMovie = (args) => {
    });
   };
 
+  export const getGenres = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US` 
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+
+
   export const getMovieReviews = ({ queryKey }) => {
     const [, idPart] = queryKey;
     const { id } = idPart;
